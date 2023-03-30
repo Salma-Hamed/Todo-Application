b51ae8bd2b76a5ddb4585738df25a592464266a7
Salma-Hamed <71777717+Salma-Hamed@users.noreply.github.com>
2023-03-30 15:42:56 +0200

    Create TodoItem.java

---
+++ TodoItem.java
@@ -0,0 +1,42 @@
+package todo;
+import java.util.ArrayList;
+public class TodoItem{
+    
+    private String title;
+    private String description;
+    private int priority;
+    private ArrayList category;
+    private String start;
+    private String end;
+
+    public TodoItem(String t, String desc, int p, ArrayList cat, String st, String e)
+    {
+        title = t;
+        description = desc;
+        priority = p;
+        category = cat;
+        start = st;
+        end =  e;
+    }
+
+    public static void updateItem(String t, String desc, int p, ArrayList cat, String st, String e)
+    {
+        if(title == t)
+        {
+            description = desc;
+            priority = p;
+            category = cat;
+            start = st;
+            end =  e;
+        }
+        else
+        {
+            throw new ItemNotFound();
+        }
+    }
+    public static void addCat(String cat)
+    {
+        category.add(cat);
+    }
+
+}
