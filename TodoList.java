9cd298003cc211974fc72b1a1956d6462114e6b9
Salma-Hamed <71777717+Salma-Hamed@users.noreply.github.com>
2023-03-30 15:43:39 +0200

    Create TodoList.java

---
+++ TodoList.java
@@ -0,0 +1,28 @@
+package todo;
+import java.util.ArrayList;
+import java.util.Scanner;
+public class TodoList{
+    public static void main(String []args)
+    {
+        Scanner s = new Scanner(System.in);
+        ArrayList <TodoItem> allItems = new TodoItem <>();
+        
+        System.out.Println("Choose one of the following features:");
+        System.out.Println("1- Add item");
+        System.out.Println("2- Update item");
+        System.out.Println("3- Delete item");
+        System.out.Println("4- Show all items");
+        System.out.Println("5- Show top 5 nearest by date");
+        System.out.Println("6- Search by title");
+        System.out.println("7- Search by start date");
+        System.out.println("8- Search by end date");
+        System.out.println("9- Add item to a category");
+        System.out.println("10- Add item to a favorite");
+
+        int option = s.nextInt();
+
+        // check input 1 : 10
+        if(option < 1 || option > 10)
+        {}
+    }
+}
